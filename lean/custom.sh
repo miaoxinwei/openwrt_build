05304a034edac1c77ffde11d640ad6fc30fecda1
lico <254975828@qq.com>
2020-04-13 11:08:18 +0800

    Update custom.sh

--- lean/custom.sh
+++ lean/custom.sh
@@ -2,5 +2,4 @@ echo -e "\nnet.ipv4.tcp_fastopen = 3" >> package/base-files/files/etc/sysctl.con
 echo -e "net.core.netdev_max_backlog = 32768" >> package/base-files/files/etc/sysctl.conf
 echo -e "net.ipv4.tcp_max_syn_backlog = 16384" >> package/base-files/files/etc/sysctl.conf
 echo -e "net.core.somaxconn = 32768" >> package/base-files/files/etc/sysctl.conf
-echo -e "net.ipv4.tcp_tw_reuse = 1" >> package/base-files/files/etc/sysctl.conf
 echo -e "net.ipv4.tcp_fin_timeout = 30" >> package/base-files/files/etc/sysctl.conf
